28073947133676066fc6cd3fc7c1e2f86fcb6780
Lorin Vandegrift <lt_vandegrift@live.com>
2017-04-16 16:00:29 -0700

    starting to work on 5x5 grid

---
+++ simpleArmEnv/9_action.py
@@ -0,0 +1,105 @@
+import sys
+sys.path.insert(0, 'hardware')
+
+from driver import Driver
+from ax12 import *
+from time import sleep
+MOTOR_SPEED = 35
+MAX_LIMIT = 650
+MIN_LIMIT = 450
+NO_MOVE = 0
+GO_LEFT = 1
+GO_RIGHT = 2
+GO_UP = 3
+GO_DOWN = 4
+BASE_MOTOR_STEP =70
+WRIST_MOTOR_STEP = 30
+
+class ArmActions:
+
+    def __init__(self):        
+        self.driver = Driver()
+        sleep(5)  
+        self.setup_motors()
+        self.actions = [[0,0],[-BASE_MOTOR_STEP,0],[BASE_MOTOR_STEP,0],[0,WRIST_MOTOR_STEP],[0,-WRIST_MOTOR_STEP]] 
+        self.num_actions = len(self.actions)   
+        self.inital_pose = []
+        for i in range(4):  
+            self.inital_pose.append(self.driver.getReg(i+1, P_PRESENT_POSITION_L, 2))
+        print self.inital_pose   
+
+    def setup_motors(self, num_motors=4):
+        response = raw_input("Do you want to set the motors? (y/n) ")
+        if 'y' in response:
+            for i in range(num_motors):                        
+                self.driver.setReg(i+1, P_TORQUE_ENABLE, [0])
+            raw_input("Enter any value when ready.")
+            print "Setting Motors..."  
+
+        for i in range(num_motors):            
+            self.driver.setReg(i+1, P_TORQUE_ENABLE, [1])  
+
+    def get_num_actions(self):
+        return self.num_actions
+
+    def take_action(self, action_id, state):
+        if action_id == GO_LEFT:                
+            if state == 0 or state == 3 or state == 6:
+                return
+        if action_id == GO_RIGHT:                
+            if state == 2 or state == 5 or state == 8:
+                return
+        if action_id == GO_UP: 
+            if state == 0 or state == 1 or state == 2:
+                return
+        if action_id == GO_DOWN: 
+            if state == 6 or state == 7 or state == 8:
+                return    
+
+        self.set_motors(action_id)
+    
+    def set_motors(self, action_id):        
+        self.driver.setReg(1, 32, self.to_hl(MOTOR_SPEED))        
+        motor_pose = self.driver.getReg(1, P_PRESENT_POSITION_L, 2)     
+        new_pose = self.to_standard(motor_pose) + self.actions[action_id][0]             
+        if self.check_pose_limits(new_pose, motor=1):
+            self.driver.setReg(1, 30, self.to_hl(new_pose))        
+
+        self.driver.setReg(4, 32, self.to_hl(MOTOR_SPEED))        
+        motor_pose = self.driver.getReg(4, P_PRESENT_POSITION_L, 2)     
+        new_pose = self.to_standard(motor_pose) + self.actions[action_id][1]               
+        if self.check_pose_limits(new_pose, motor=4):
+            self.driver.setReg(4, 30, self.to_hl(new_pose))        
+
+    def check_pose_limits(self, pose, motor):
+        print "Motor: %d" % motor
+        print "Pose: %d" % pose
+        if motor == 1:
+            if pose > MAX_LIMIT or pose < MIN_LIMIT:
+                return False
+            else:
+                return True
+        '''
+        if motor == 4:
+            if pose > MAX_LIMIT or pose < MIN_LIMIT:
+                return False
+            else:
+                return True
+        '''
+        return True
+
+
+    def reset(self):
+        for i in range(3):
+            self.driver.setReg(1, 32, self.inital_pose[i])        
+        sleep(2)        
+
+    def to_hl(self, value):        
+        l = value & 255
+        h = value >> 8
+
+        return [l,h]
+
+    def to_standard(self, values):
+        l,h = values
+        return (h<<8) | l
---
+++ simpleArmEnv/9_state.py
@@ -0,0 +1,47 @@
+import sys
+sys.path.insert(0, 'computer_vision')
+import numpy as np
+
+from selective_search_hog import BlockDetector
+import cv2
+
+class ArmState:
+
+    def __init__(self):
+        self.detector = BlockDetector()
+        self.num_states = 10
+        self.screen_sections = 3
+
+    def get_num_states(self):
+        return self.num_states
+
+    def draw_grid_lines(self,image):        
+        for i in range(0, image.shape[1], image.shape[1]/self.screen_sections):
+            image[:,i:i+5] = 0
+        for i in range(0, image.shape[0], image.shape[0]/self.screen_sections):
+            image[i:i+5,:] = 0
+        cv2.imshow("State", image)
+        cv2.waitKey(25)
+
+        return image
+
+    def get_state(self):        
+        center, _, image = self.detector.detect()
+        image = self.draw_grid_lines(image)
+        if center[0] == -1:
+            return 9
+        width, height, ch = image.shape
+        per_row = height / self.screen_sections
+        per_col = width / self.screen_sections
+        x_cell = center[0] / per_row
+        y_cell = center[1] / per_col        
+
+        print self.screen_sections * int(y_cell) + int(x_cell)
+        return self.screen_sections * int(y_cell) + int(x_cell)
+
+    def display_state_image(self, image):
+        cv2.imshow("State", image)
+        cv2.waitKey(25)  
+
+
+
